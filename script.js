b2b7645cd6ce9ca5a7f6d02fa75ef1f039406d21
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-08-21 23:11:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const form = document.getElementById("form1") ;
+
+
 function getFormvalue() {
-    //Write your code here
+	   let fullName = form["fname"].value + " "+ form["lname"].value ;
+		alert(fullName);
 
 }
